8ea283655b5c146775209edfea04c2992fa6689c
mamamia140 <m-h-k140@hotmail.com>
2024-08-28 15:41:42 +0300

    added exit portal and other levels

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        int sceneNumber = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(IELoadTheLevel(sceneNumber+1));
+    }
+
+    IEnumerator IELoadTheLevel(int sceneNumber){
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(sceneNumber);
+
+    }
+}
